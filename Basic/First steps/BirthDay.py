d9f4c865506583f70712b251e9affe5f84a45b46
Valeri Hristov <valerimagic@gmail.com>
2020-09-19 21:42:16 +0300

    new

---
+++ Basic/First steps/BirthDay.py
@@ -0,0 +1,9 @@
+naemPrice = float(input())
+
+priceCake = naemPrice * 0.2
+priceDrink = priceCake * 0.55
+animator = naemPrice / 3
+
+needBudget = naemPrice + priceCake + priceDrink + animator
+
+print(needBudget)
