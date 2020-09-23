40c90f929ee7b1140f719b191724bd2d7100d19a
Valeri Hristov <valerimagic@gmail.com>
2020-09-23 22:30:16 +0300

    new

---
+++ Basic/If_Else_Elif/SumSeconds.py
@@ -0,0 +1,12 @@
+secondFirst = int(input())
+secondSecond = int(input())
+secondThird = int(input())
+
+sumInSeconds = secondFirst + secondSecond + secondThird
+minutes = sumInSeconds // 60
+seconds = sumInSeconds % 60
+
+if seconds < 10:
+    print(f"{minutes}:0{seconds}")
+else:
+    print(f"{minutes}:{seconds}")
