817effdb43740d90d64374ca352da762115f82e1
Valeri Hristov <valerimagic@gmail.com>
2020-09-25 22:46:18 +0300

    update

--- Basic/If_Else_Elif/BonusPoint.py
+++ Basic/If_Else_Elif/BonusPoint.py
@@ -4,13 +4,13 @@ count = number
 
 if number <= 100:
     count += 5
-elif number > 100 and number < 1000:
+elif 100 < number < 1000:
     count = number * 1.2
 elif number > 1000:
     count = number * 1.1
 
-if number % 2 == 0:
-    count += 1
+    if number % 2 == 0:
+        count += 1
 if number % 10 == 5:
     count += 2
 
